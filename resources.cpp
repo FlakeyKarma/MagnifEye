0c13ffdeef6f554e74713f7d25a0eee09da6b6ad
Flakey Karma <flakeykarma21@gmail.com>
2019-04-23 15:16:44 -0600

    Added FileNotFound for menu if file isn't found

--- resources.cpp
+++ resources.cpp
@@ -78,62 +78,69 @@ void stuffReturn::outP(string file, int ac) {
 	string inpt, inpt0, txtF = file;
 	ifstream fil;
 	fil.open(txtF);
-	while (!fil.eof()) {
-		getline(fil, inpt0);
-		inpt += inpt0 + " \n";
-	}
-	stuffReturn sR;
-	vector<string> a = sR.wordReturn(inpt);
-	sR.nuMake(a);
-	a = sR.strReturn();
-	vector<long> a0 = sR.numzReturn();
-	long COUNT = 0;
-	while (COUNT < a.size()) {
-		sR.progressBar(float(1.0/a.size()*20.0));
-		for (long i = 0; i < a.size(); i++) {
-			if (i < a.size()) {
-				if ((i + 1) >= a0.size())
-					break;
-				if (a0.at(i) > a0.at(i + 1)) {
-					iter_swap(a.begin() + i, a.begin() + i + 1);
-					iter_swap(a0.begin() + i, a0.begin() + i + 1);
+	bool isGud = true;
+	if(!fil.good()){
+			cout << "-+=FileNotFound=+-" << endl;
+			isGud = false;
+		}
+	if(isGud){
+		while (!fil.eof()) {
+			getline(fil, inpt0);
+			inpt += inpt0 + " \n";
+		}
+		stuffReturn sR;
+		vector<string> a = sR.wordReturn(inpt);
+		sR.nuMake(a);
+		a = sR.strReturn();
+		vector<long> a0 = sR.numzReturn();
+		long COUNT = 0;
+		while (COUNT < a.size()) {
+			sR.progressBar(float(1.0/a.size()*20.0));
+			for (long i = 0; i < a.size(); i++) {
+				if (i < a.size()) {
+					if ((i + 1) >= a0.size())
+						break;
+					if (a0.at(i) > a0.at(i + 1)) {
+						iter_swap(a.begin() + i, a.begin() + i + 1);
+						iter_swap(a0.begin() + i, a0.begin() + i + 1);
+					}
 				}
 			}
+			COUNT++;
 		}
-		COUNT++;
-	}
-	int w0 = 0, w1 = 0;
-	while (a.size() - 1 > w0) {
-		w1 = w0;
-		while (w1 < a.size()) {
-			if (a.at(w0) == a.at(w1)) {
-				a.erase(a.begin() + w1);
-				a0.erase(a0.begin() + w1);
+		int w0 = 0, w1 = 0;
+		while (a.size() - 1 > w0) {
+			w1 = w0;
+			while (w1 < a.size()) {
+				if (a.at(w0) == a.at(w1)) {
+					a.erase(a.begin() + w1);
+					a0.erase(a0.begin() + w1);
+				}
+				w1++;
 			}
-			w1++;
+			w0++;
 		}
-		w0++;
-	}
-	cout << "---------" << endl;
-	long SET, numS;
-	long LENg = 0, i = 0;
-	string SP = "        ";
-	while (true) {
-		SET = a0.at(i);
-		cout << "OUTPUT " << "=| " << a0.at(i) << " | :" << endl;
-		while (SET == a0.at(i)) {
-			SP = "        ";
-			for (int i = 0; i < a0.size(); i++) {
-				SP += " ";
+		cout << "---------" << endl;
+		long SET, numS;
+		long LENg = 0, i = 0;
+		string SP = "        ";
+		while (true) {
+			SET = a0.at(i);
+			cout << "OUTPUT " << "=| " << a0.at(i) << " | :" << endl;
+			while (SET == a0.at(i)) {
+				SP = "        ";
+				for (int i = 0; i < a0.size(); i++) {
+					SP += " ";
+				}
+				cout << SP << a.at(i) << endl;
+				i++;
+				if (a0.size() <= i)
+					break;
 			}
-			cout << SP << a.at(i) << endl;
-			i++;
-			if (a0.size() <= i)
+			LENg++;
+			if (LENg == a.size() || i >= a0.size())
 				break;
 		}
-		LENg++;
-		if (LENg == a.size() || i >= a0.size())
-			break;
 	}
 }
 
