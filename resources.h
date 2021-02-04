61c4cc00a620c67aad599c6f6605444b65709f56
FlakeyKarma <flakeykarma21@gmail.com>
2021-02-04 01:25:40 -0700

    Forgot to update version info.

--- resources.h
+++ resources.h
@@ -35,7 +35,7 @@ class Complementary {
 		float progress = 0;
 		int chk = 0, numCheck = 0, prg = 0, metricz[2] = {5,10};
 		std::string loadBar, file0="", file1="", inptMain;
-		const std::string verz = "v2.26.12";
+		const std::string verz = "v2.27.12";
 		bool tw = 0, th = 0, prd = 0, libUse, exclude_set = 0, removal_set = 0, delimeter_set = 0, param_bool[2] = {0};
 		char *removal = (char *)malloc(1), *delimeterz = (char *)malloc(1), *etc_path = (char *)malloc(_HDN_LENG_ + _HDN_LENG_EXT_);
 		std::vector<char *> *exclusionz, *inclusionz;
